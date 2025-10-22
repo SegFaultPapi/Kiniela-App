d1a3ffc6d455b4252dd3f1ae290a451af406867c
SegFaultPapi <0xandres.rmdo@gmail.com>
2025-10-21 22:28:18 -0600

    chore(blockchain): initialize Hardhat config for Base Sepolia (ST-003)

---
+++ contracts/deploy.sh
@@ -0,0 +1,6 @@
+forge script script/Counter.s.sol:CounterScript \
+--rpc-url https://base-sepolia.g.alchemy.com/v2/3KALM7E58TlP6f6GT5vf6 \
+--account DevelopmentAcc \
+--broadcast \
+--verify \
+--etherscan-api-key MWQ3UK36C2TXZ97XXR4AY8W78Y84Y87T9V 
